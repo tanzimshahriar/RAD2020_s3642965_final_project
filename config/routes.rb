58022a8f9a27067fbcd94b45583815bae645157c
tanzimshahriar <tanzimshahriarfahim@gmail.com>
2020-06-13 07:09:02 +1000

    can be added and deleted

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'showcase#index'
   post 'toggle_time_format' => 'showcase#toggle_time_format'
   post 'change_theme' => 'showcase#change_theme'
+  post 'add_city' => 'showcase#add_city'
+  post 'del_city' => 'showcase#del_city'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
